b8fb960f2ec1060474b0297a7bced7c2c8621d90
Serkan Eken <serkanatwork@gmail.com>
2019-12-14 19:00:55 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("./models/Todo");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+require("./routes")(app);
+
+
+app.listen(5000 || 5000, () => {
+  console.log("Server is running on " + port);
+});
