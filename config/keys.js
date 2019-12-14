221724887a51c6b2d57a3911075317e4d65c2b9e
Serkan Eken <serkanatwork@gmail.com>
2019-12-15 01:19:57 +0300

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb://heroku_k5bdv510:zxcvb140@ds343217.mlab.com:43217/heroku_k5bdv510'
+  mongoURI: 'mongodb://admin:zxcvb140@ds343217.mlab.com:43217/heroku_k5bdv510'
 };
