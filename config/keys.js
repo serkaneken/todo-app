c283e5f4580b396f3036853478f9d0f121e960a5
Serkan Eken <serkanatwork@gmail.com>
2019-12-15 14:57:10 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:zxcvb140@ds343217.mlab.com:43217/heroku_k5bdv510'
+  mongoURI: 'mongodb://{username}:{password}@ds343217.mlab.com:43217/heroku_k5bdv510'
 };
