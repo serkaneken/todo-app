04d31812888809de6e76be0b2a8c05f430c1eac0
Serkan Eken <serkanatwork@gmail.com>
2019-12-15 20:09:29 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 require("./models/Todo");
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+mongoose.connect(keys.mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
-const db = require("./config/keys").mongoURI;
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-  .then(console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
+mongoose.set("useFindAndModify", false);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./routes")(app);
 
-const port = 5000;
-app.listen(port, () => {
-  console.log("Server is running on " + port);
+app.use(express.static("client/build"));
+
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
